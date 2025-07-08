ca9363987ed6d219344d8d55a46cac0f43c4efcb
Jean-Frederic Clere <jfclere@gmail.com>
2025-07-08 06:17:43 -0700

    Arrange to use the jbcs internal repo for windows.

--- httpd24.bat
+++ httpd24.bat
@@ -1,12 +1,16 @@
 set OPENSSL_VERSION=3.5.0
 set BASEDIR=%CD%
 
-curl -L -o httpd24.tar.gz https://dist.apache.org/repos/dist/dev/httpd/httpd-2.4.64-rc2.tar.gz
-tar xzvf httpd24.tar.gz
-call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=x64
+rmdir /s /q httpd
+git -c http.sslVerify=false clone https://gitlab.cee.redhat.com/jboss-web-server/jbcs/httpd
+cd httpd
+git checkout 2.4.62
+cd %BASEDIR%
 rmdir /s /q httpd-build
 mkdir httpd-build
 cd httpd-build
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=x64
+set CFLAGS=/D "HTTPD_ROOT=\"C:/APR\"" /D "SERVER_CONFIG_FILE=\"C:/APR/conf/httpd.conf\""
 cmake -G "Visual Studio 17 2022" ^
 -DAPR_INCLUDE_DIR=C:/APR/include ^
 -DAPR_LIBRARIES=C:/APR/lib/libapr-1.lib;C:/APR/lib/libaprapp-1.lib;C:/APR/lib/apr_ldap-1.lib;C:/APR/lib/libaprutil-1.lib ^
@@ -15,7 +19,7 @@ cmake -G "Visual Studio 17 2022" ^
 -DCMAKE_LIBRARY_PATH_FLAG="C:/OPENSSL/bin" ^
 -DOPENSSL_LIBRARIES="C:/OPENSSL/lib/libssl.lib;C:/OPENSSL/lib/libcrypto.lib" ^
 -DOPENSSL_INCLUDE_DIR="C:/OPENSSL/include" ^
-%BASEDIR%\httpd-2.4.64
+%BASEDIR%\httpd
 
 MSBuild libhttpd.vcxproj -t:build -p:Configuration=Release
 MSBuild httpd.vcxproj -t:build -p:Configuration=Release
